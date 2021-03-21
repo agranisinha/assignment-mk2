a376a6bc744f407ed5f45044224421582fccc674
tli146 <36920783+tli146@users.noreply.github.com>
2021-03-21 22:41:32 +1000

    yadayada

--- src/towersim/tasks/Task.java
+++ src/towersim/tasks/Task.java
@@ -1,5 +1,24 @@
 package towersim.tasks;
 
 public class Task {
-
+    private int loadPercentage;
+    private TaskType type;
+    public Task(TaskType type){
+        this.type = type;
+        loadPercentage = 0;
+    }
+    public Task(TaskType type, int loadPercentage){
+        this.type = type;
+        loadPercentage = loadPercentage;
+    }
+    public TaskType getType(){
+        return type;
+    }
+    public int getLoadPercent(){
+        return loadPercentage;
+    }
+    public String toString(){
+        return "";
+        //placeholder
+    }
 }
--- src/towersim/tasks/TaskList.java
+++ src/towersim/tasks/TaskList.java
@@ -1,5 +1,33 @@
 package towersim.tasks;
+import java.util.*;
 
 public class TaskList {
+    private ArrayList<Task> listOfTasks;
+    private int currentTask = 0;
+    public TaskList(List<Task> tasks){
+        listOfTasks = (ArrayList<Task>)tasks;
+    }
+    public Task getCurrentTask(){
+        return listOfTasks.get(currentTask);
+    }
+    public Task getNextTask(){
+        if(currentTask == listOfTasks.size()){
+            return listOfTasks.get(0);
+        }else {
+            return listOfTasks.get(currentTask + 1);
+        }
+    }
+    public void moveToNextTask(){
+        currentTask++;
+            if(currentTask == listOfTasks.size()){
+                currentTask = 0;
+            }
+    }
+    public String toString(){
+        //placeholder
+        return "";
+    }
+
+
 
 }
