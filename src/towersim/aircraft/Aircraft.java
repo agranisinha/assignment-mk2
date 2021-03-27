a2bd4c2ff722fc67940538aefabc366cc915fa79
tli146 <36920783+tli146@users.noreply.github.com>
2021-03-27 10:30:28 +1000

    yadayada

--- src/towersim/aircraft/Aircraft.java
+++ src/towersim/aircraft/Aircraft.java
@@ -3,9 +3,9 @@ package towersim.aircraft;
 import towersim.util.*;
 import towersim.tasks.*;
 
-public abstract class Aircraft extends Object implements EmergencyState, OccupancyLevel, Tickable {
+public abstract class Aircraft implements EmergencyState, OccupancyLevel, Tickable {
     static double LITRE_OF_FUEL_WEIGHT = 0.8;
-    ;
+
 
     private boolean emergencyState;
     //the state of emergency of the aircraft
@@ -18,6 +18,13 @@ public abstract class Aircraft extends Object implements EmergencyState, Occupan
     private double fuelAmount;
     //fuel amount in Litres
 
+    /**
+     * Constructor
+     * @param callsign callsign of aircraft
+     * @param characteristics enum aircraftcharacteristics including model, make, capacity etc
+     * @param tasks list of future tasks to complete
+     * @param fuelAmount current fuel amount of the aircraft
+     */
     protected Aircraft(String callsign, AircraftCharacteristics characteristics, TaskList tasks, double fuelAmount) {
         this.callSign = callsign;
         this.characteristics = characteristics;
@@ -26,6 +33,7 @@ public abstract class Aircraft extends Object implements EmergencyState, Occupan
         this.fuelAmount = fuelAmount;
         //constructs the object
     }
+
 
     public void declareEmergency() {
         emergencyState = true;
@@ -41,6 +49,8 @@ public abstract class Aircraft extends Object implements EmergencyState, Occupan
     //emergency state functions, overriding emergency state interface.
 
     //getters
+
+
     public String getCallsign() {
         return callSign;
     }
