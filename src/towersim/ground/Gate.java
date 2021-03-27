f06cbce04f9bbd3c115c7681485370c16de2c482
tli146 <36920783+tli146@users.noreply.github.com>
2021-03-27 10:16:59 +1000

    yadayada

--- src/towersim/ground/Gate.java
+++ src/towersim/ground/Gate.java
@@ -1,9 +1,41 @@
 package towersim.ground;
 
+import towersim.aircraft.Aircraft;
+
 public class Gate {
     private int gateNumber;
+    private Aircraft aircraftAtGate = null;
+
     public Gate(int gateNumber){
         this.gateNumber = gateNumber;
+    }
+
+    public void parkAircraft(Aircraft aircraft){
+        this.aircraftAtGate = aircraft;
+        //parks a new aircraft at this gate
+    }
+
+    public void aircraftLeaves(){
+        aircraftAtGate = null;
+    }
+
+    public Aircraft getAircraftAtGate() {
+        return aircraftAtGate;
+    }
+
+    public boolean isOccupied(){
+        if(aircraftAtGate == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public int getGateNumber(){
+        return gateNumber;
+    }
+
+    public String toString(){
+        return "";
     }
 
 }
--- src/towersim/ground/Terminal.java
+++ src/towersim/ground/Terminal.java
@@ -1,5 +1,20 @@
 package towersim.ground;
 
-public abstract class Terminal {
+import towersim.util.EmergencyState;
+import towersim.util.OccupancyLevel;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Terminal implements EmergencyState, OccupancyLevel {
+    static int MAX_NUM_GATES = 6;
+    private List<Gate> gatesList = new ArrayList<Gate>();
+    int terminalNumber;
+
+    protected Terminal(int terminalNumber){
+        this.terminalNumber = terminalNumber;
+    }
+
+
 
 }
